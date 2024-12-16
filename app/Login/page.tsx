bd9324853cf6e24c6e7cd20aabcdcb1e8a514985
Asma-Afzal123 <162578850+Asma-Afzal123@users.noreply.github.com>
2024-12-16 20:51:49 +0500

    pushed

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Login from '../Components/Login';
+// import React from 'react';
+// import Login from '../Components/Login';
 
-const login = () => {
-  return (
-    <Login />
-  )
-};
+// const login = () => {
+//   return (
+//     <Login />
+//   )
+// };
 
-export default Login;
+// export default Login;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./Navbar/Navbar";
+
 import Footer from "./Footer/Footer";
 
 const geistSans = localFont({
